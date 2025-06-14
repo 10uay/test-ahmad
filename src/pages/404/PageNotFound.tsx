1231de0b236de62da76cc5e5c8f6d360ee4b3c84
Louay <Louay.deeb45@gmail.com>
2025-06-14 22:12:36 +0300

    14-06-2025

--- src/pages/404/PageNotFound.tsx
+++ src/pages/404/PageNotFound.tsx
@@ -2,10 +2,10 @@ import FuzzyText from "@/components/bits/TextAnimations/FuzzyText/FuzzyText";
 
 export default function PageNotFound() {
   return (
-    <div className="bg-black mt-40 p-4 flex flex-col sm:max-w-[600px] max-w-[220px]">
+    <div className="bg-black mt-40 p-4 flex flex-col sm:max-w-[600px] max-w-[220px] container mx-auto">
       <FuzzyText
         baseIntensity={0.1}
-        className=" pb-4">
+        className="pb-4">
         404
       </FuzzyText>
 
